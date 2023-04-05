1d03a568d4b1aa2359b18b3bfd7019415127ac5f
rubenTome <rubentome27@gmail.com>
2023-04-05 13:21:24 +0200

    comentario

--- python_code/mqttClient.py
+++ python_code/mqttClient.py
@@ -1,5 +1,8 @@
 import paho.mqtt.client as mqtt
 
+#tendremos un cliente para crear y publicar las particiones
+#los clientes restantes se subscriben a su particion y publican los resultados
+
 #se llama al conectarse al broker
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -15,6 +18,6 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 #conectamos con el broker
-client.connect("localhost")#debe ser el nombre o ip
+client.connect("192.168.1.138", 1883)#debe ser el nombre o ip
 
 client.loop_forever()
