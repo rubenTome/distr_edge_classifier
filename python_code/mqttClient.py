2424f89024e655e87a5f27a1da7e31d398241b08
rubenTome <rubentome27@gmail.com>
2023-04-05 11:27:21 +0200

    mqtt cliente basico y broker vacio

---
+++ python_code/mqttClient.py
@@ -0,0 +1,20 @@
+import paho.mqtt.client as mqtt
+
+#se llama al conectarse al broker
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe("partition/1") #nos subscribimos a este tema
+
+#se llama al obtener un mensaje del broker
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload)) #imprimimos la respuesta
+    client.publish("partition/1/results", "resultados1")#publicamos los resultados
+
+client = mqtt.Client("client1")
+client.on_connect = on_connect
+client.on_message = on_message
+
+#conectamos con el broker
+client.connect("localhost")#debe ser el nombre o ip
+
+client.loop_forever()
