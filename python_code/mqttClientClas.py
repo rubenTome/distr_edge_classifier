2a66c2f0315fa6a758b696164599724d8a6e054c
rubenTome <rubentome27@gmail.com>
2023-04-05 19:30:48 +0200

    prints y comentarios en cliente clasif

--- python_code/mqttClientClas.py
+++ python_code/mqttClientClas.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 
 #los clientes se subscriben a su particion y publican los resultados
+#ARRANCAR PRIMERO LOS CLASIFICADORES
 BROKER_IP = "192.168.1.138"
 
 #se llama al conectarse al broker
@@ -12,6 +13,7 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload)) #imprimimos la respuesta
     client.publish("partition/results/1", "results_clas_client_1")#publicamos los resultados
+    print("Published results: results_clas_client_1")
 
 client = mqtt.Client("clas_client_1")
 client.on_connect = on_connect
