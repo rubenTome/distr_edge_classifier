be0a6cb8be4f7129bdd78023cfaba74a68d4fbc6
rubenTome <rubentome27@gmail.com>
2023-04-07 13:01:57 +0200

    descomentadas lineas clasficar,  se crea con ellas funcion classifier

--- python_code/mqttClientClas.py
+++ python_code/mqttClientClas.py
@@ -11,112 +11,122 @@ from prettytable import PrettyTable
 #los clientes se subscriben a su particion y publican los resultados
 #ARRANCAR PRIMERO LOS CLASIFICADORES
 
-# #CLASIFICADORES
+ #CLASIFICADORES
 
-# def knn(partition):#partition es un pandas.DataFrame
-#     partition = partition.to_numpy()
-#     nVars = np.shape(partition)[1] - 1
-#     trainset = partition[:, np.arange(nVars)]
-#     trainclasses = partition[:,[nVars]].flatten()
-#     clf = KNeighborsClassifier(n_neighbors = 2)
-#     clf.fit(trainset, trainclasses)
-#     score = clf.score(ds["testset"].to_numpy(), ds["testclasses"].to_numpy().flatten())
-#     if(len(np.unique(trainclasses)) == 2):
-#         av = "binary"
-#     else:
-#         av = "weighted"
-#     precision = precision_score(ds["testclasses"].to_numpy().flatten(), 
-#                                 clf.predict(ds["testset"].to_numpy()), average=av)
-#     recall = recall_score(ds["testclasses"].to_numpy().flatten(), 
-#                           clf.predict(ds["testset"].to_numpy()), average=av)
+def knn(partition):#partition es un pandas.DataFrame
+    partition = partition.to_numpy()
+    nVars = np.shape(partition)[1] - 1
+    trainset = partition[:, np.arange(nVars)]
+    trainclasses = partition[:,[nVars]].flatten()
+    clf = KNeighborsClassifier(n_neighbors = 2)
+    clf.fit(trainset, trainclasses)
+    score = clf.score(ds["testset"].to_numpy(), ds["testclasses"].to_numpy().flatten())
+    if(len(np.unique(trainclasses)) == 2):
+        av = "binary"
+    else:
+        av = "weighted"
+    precision = precision_score(ds["testclasses"].to_numpy().flatten(), 
+                                clf.predict(ds["testset"].to_numpy()), average=av)
+    recall = recall_score(ds["testclasses"].to_numpy().flatten(), 
+                        clf.predict(ds["testset"].to_numpy()), average=av)
 
-#     return [score, precision, recall]
+    return [score, precision, recall]
 
-# def rf(partition):
-#     partition = partition.to_numpy()
-#     nVars = np.shape(partition)[1] - 1
-#     trainset = partition[:, np.arange(nVars)]
-#     trainclasses = partition[:,[nVars]].flatten()
-#     rfc = RandomForestClassifier()
-#     rfc.fit(trainset, trainclasses)
-#     scores = rfc.score(ds["testset"].to_numpy(), ds["testclasses"].to_numpy().flatten())
-#     if(len(np.unique(trainclasses)) == 2):
-#         av = "binary"
-#     else:
-#         av = "weighted"
-#     precision = precision_score(ds["testclasses"].to_numpy().flatten(), 
-#                                 rfc.predict(ds["testset"].to_numpy()), average=av)
-#     recall = recall_score(ds["testclasses"].to_numpy().flatten(), 
-#                           rfc.predict(ds["testset"].to_numpy()), average=av)
-#     return [scores, precision, recall]
+def rf(partition):
+    partition = partition.to_numpy()
+    nVars = np.shape(partition)[1] - 1
+    trainset = partition[:, np.arange(nVars)]
+    trainclasses = partition[:,[nVars]].flatten()
+    rfc = RandomForestClassifier()
+    rfc.fit(trainset, trainclasses)
+    scores = rfc.score(ds["testset"].to_numpy(), ds["testclasses"].to_numpy().flatten())
+    if(len(np.unique(trainclasses)) == 2):
+        av = "binary"
+    else:
+        av = "weighted"
+    precision = precision_score(ds["testclasses"].to_numpy().flatten(), 
+                                rfc.predict(ds["testset"].to_numpy()), average=av)
+    recall = recall_score(ds["testclasses"].to_numpy().flatten(), 
+                        rfc.predict(ds["testset"].to_numpy()), average=av)
+    return [scores, precision, recall]
 
-# def xgb(partition):
-#     partition = partition.to_numpy()
-#     nVars = np.shape(partition)[1] - 1
-#     trainset = partition[:, np.arange(nVars)]
-#     trainclasses = partition[:,[nVars]].flatten()
-#     gbc = GradientBoostingClassifier()
-#     gbc.fit(trainset, trainclasses)
-#     scores = gbc.score(ds["testset"].to_numpy(), ds["testclasses"].to_numpy().flatten())
-#     if(len(np.unique(trainclasses)) == 2):
-#         av = "binary"
-#     else:
-#         av = "weighted"
-#     precision = precision_score(ds["testclasses"].to_numpy().flatten(), 
-#                                 gbc.predict(ds["testset"].to_numpy()), average=av)
-#     recall = recall_score(ds["testclasses"].to_numpy().flatten(), 
-#                           gbc.predict(ds["testset"].to_numpy()), average=av)
-#     return [scores, precision, recall]
+def xgb(partition):
+    partition = partition.to_numpy()
+    nVars = np.shape(partition)[1] - 1
+    trainset = partition[:, np.arange(nVars)]
+    trainclasses = partition[:,[nVars]].flatten()
+    gbc = GradientBoostingClassifier()
+    gbc.fit(trainset, trainclasses)
+    scores = gbc.score(ds["testset"].to_numpy(), ds["testclasses"].to_numpy().flatten())
+    if(len(np.unique(trainclasses)) == 2):
+        av = "binary"
+    else:
+        av = "weighted"
+    precision = precision_score(ds["testclasses"].to_numpy().flatten(), 
+                                gbc.predict(ds["testset"].to_numpy()), average=av)
+    recall = recall_score(ds["testclasses"].to_numpy().flatten(), 
+                          gbc.predict(ds["testset"].to_numpy()), average=av)
+    return [scores, precision, recall]
 
 
-# #PARAMETROS 
+#PARAMETROS 
 
-# totalresults = None
+totalresults = None
 
-# #generar tablas con los resultados (+legible)
-# generateTables = True
+#numero de cifras decimales
+NDECIMALS = 2
 
-# classifiers = [knn, rf, xgb]
+#generar tablas con los resultados (+legible)
+generateTables = True
 
-# #names for printing them
-# namesclassifiers = ["KNN", "RF", "XGB"] 
+classifiers = [knn, rf, xgb]
+
+#names for printing them
+namesclassifiers = ["KNN", "RF", "XGB"] 
 
 BROKER_IP = "192.168.1.138"
 
-# #CREACION DE CLASIFICADORES
-# classifAcc = {}
-# for ps in range(len(Pset)):
-#     for c in range(len(classifiers)):
-#         for pa in range(len(partitions[ps])):
-#             classifAcc[namesclassifiers[c] + "_" + str(Pset[ps]) + "_" + str(pa + 1)] = (
-#             classifiers[c](partitions[ps][pa]))
+#CREACION DE CLASIFICADORES
 
-# #guardamos en un csv la informacion de cada clasificador en classifAcc
-# splitedPath = datasets[d].split("/")
-# name = splitedPath[len(splitedPath) - 1]
-# file = open("rdos_python/rdos_" + name, "w")
-# if generateTables:
-#     fileTable = open("rdos_python/tabla_rdos_" + name.split(".")[0] + ".txt", "w")
-#     headers = PrettyTable(["classifier", "npartitions", "partition", "accurancy", "precision", "recall", "energy"])
-#     headers.align = "l"
-# writer = csv.writer(file)
-# writer.writerow(["classifier", "npartitions", "partition", "accurancy", "precision", "recall", "energy"])
-# for t in range(len(classifAcc)):
-#     label = list(classifAcc)[t]
-#     classifier = classifAcc[label]
-#     splitedName = label.split("_")
-#     actualData = partitions[Pset.index(int(splitedName[1]))][int(splitedName[2]) - 1].to_numpy()
-#     actualData = actualData[:, np.arange(np.shape(actualData)[1] - 1)]
-#     energyDist = partf.end(actualData, ds["trainset"].values.tolist(), True)
-#     if generateTables:
-#         headers.add_row([splitedName[0], splitedName[1], splitedName[2], 
-#                         round(classifier[0], NDECIMALS), round(classifier[1], NDECIMALS), 
-#                         round(classifier[2], NDECIMALS), round(energyDist, NDECIMALS)])
-#     writer.writerow([splitedName[0], splitedName[1], splitedName[2], 
-#                     round(classifier[0], NDECIMALS), round(classifier[1], NDECIMALS), 
-#                     round(classifier[2], NDECIMALS), round(energyDist, NDECIMALS)])
-# if generateTables:
-#     fileTable.write(str(headers))
+#partitions: datos de la particion asignada
+#Pset: numero de particiones que queremos crear
+#datasets: nombres de los datasets usados para las particiones
+#d: indice del array datasets 
+#ds: conjunto total de todos los datos seleccionados del dataset
+def classifier(partitions, Pset, datasets, d, ds):
+    classifAcc = {}
+    for ps in range(len(Pset)):
+        for c in range(len(classifiers)):
+            for pa in range(len(partitions[ps])):
+                classifAcc[namesclassifiers[c] + "_" + str(Pset[ps]) + "_" + str(pa + 1)] = (
+                classifiers[c](partitions[ps][pa]))
+
+    #guardamos en un csv la informacion de cada clasificador en classifAcc
+    splitedPath = datasets[d].split("/")
+    name = splitedPath[len(splitedPath) - 1]
+    file = open("rdos_python/rdos_" + name, "w")
+    if generateTables:
+        fileTable = open("rdos_python/tabla_rdos_" + name.split(".")[0] + ".txt", "w")
+        headers = PrettyTable(["classifier", "npartitions", "partition", "accurancy", "precision", "recall", "energy"])
+        headers.align = "l"
+    writer = csv.writer(file)
+    writer.writerow(["classifier", "npartitions", "partition", "accurancy", "precision", "recall", "energy"])
+    for t in range(len(classifAcc)):
+        label = list(classifAcc)[t]
+        classifier = classifAcc[label]
+        splitedName = label.split("_")
+        actualData = partitions[Pset.index(int(splitedName[1]))][int(splitedName[2]) - 1].to_numpy()
+        actualData = actualData[:, np.arange(np.shape(actualData)[1] - 1)]
+        energyDist = partf.end(actualData, ds["trainset"].values.tolist(), True)
+        if generateTables:
+            headers.add_row([splitedName[0], splitedName[1], splitedName[2], 
+                            round(classifier[0], NDECIMALS), round(classifier[1], NDECIMALS), 
+                            round(classifier[2], NDECIMALS), round(energyDist, NDECIMALS)])
+        writer.writerow([splitedName[0], splitedName[1], splitedName[2], 
+                        round(classifier[0], NDECIMALS), round(classifier[1], NDECIMALS), 
+                        round(classifier[2], NDECIMALS), round(energyDist, NDECIMALS)])
+    if generateTables:
+        fileTable.write(str(headers))
 
 #MQTT
 #se llama al conectarse al broker
@@ -127,6 +137,7 @@ def on_connect(client, userdata, flags, rc):
 #se llama al obtener un mensaje del broker
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload)) #imprimimos la respuesta
+
     client.publish("partition/results/1", "results_clas_client_1")#publicamos los resultados
     print("Published results: results_clas_client_1")
 
