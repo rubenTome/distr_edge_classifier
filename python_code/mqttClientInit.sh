292214cc3beb5b1cd4a6046eb71a515604a5fc4f
rubenTome <rubentome27@gmail.com>
2023-05-08 17:29:13 +0200

    clasificadores usan random forest ahora

--- python_code/mqttClientInit.sh
+++ python_code/mqttClientInit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientClas.py 4.0 xgb; exec bash"
-gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientClas.py 4.1 xgb; exec bash"
-gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientClas.py 4.2 xgb; exec bash"
-gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientClas.py 4.3 xgb; exec bash"
-gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientClas.py 1.0 xgb; exec bash"
+gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientClas.py 4.0 rf; exec bash"
+gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientClas.py 4.1 rf; exec bash"
+gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientClas.py 4.2 rf; exec bash"
+gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientClas.py 4.3 rf; exec bash"
+gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientClas.py 1.0 rf; exec bash"
 sleep 2
 gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientPart.py [4,1] /home/ruben/FIC/Q8/TFG/clean_partition/scenariosimul/scenariosimulC8D3G3STDEV0.05.csv; exec bash"
