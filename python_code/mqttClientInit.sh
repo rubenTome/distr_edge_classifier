3e6920ef74dbad41c2c8cd37c96d736cfd20cd8f
rubenTome <rubentome27@gmail.com>
2023-06-26 20:35:43 +0200

    rdos son normales. Cambiamos simul .15 por simul .05

--- python_code/mqttClientInit.sh
+++ python_code/mqttClientInit.sh
@@ -3,4 +3,4 @@ gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python
 gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientClas.py 3.1 rf 192.168.1.141; exec bash"
 gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientClas.py 3.2 rf 192.168.1.141; exec bash"
 sleep 5
-gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientPart.py [3] 3000 500 balanced random /home/ruben/FIC/Q8/TFG/clean_partition/scenariosimul/scenariosimulC8D5G3STDEV0.15.csv; exec bash"
+gnome-terminal -- bash -c "python3 /home/ruben/FIC/Q8/TFG/clean_partition/python_code/mqttClientPart.py [3] 3000 500 balanced random /home/ruben/FIC/Q8/TFG/clean_partition/scenariosimul/HIGGS.csv; exec bash"
