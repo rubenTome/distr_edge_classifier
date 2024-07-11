4d5f607f174249039b99bdff79d5d1f49d2f3b7a
lidia <ruben.tome@udc.es>
2024-07-11 13:22:54 +0200

    cambios central loop

--- python_code_v2/centralLoop.py
+++ python_code_v2/centralLoop.py
@@ -5,10 +5,10 @@ import time
 nNodes = sys.argv[1]
 train = "0.75"
 test = "0.25"
-nDatas = ["1500"]
+nDatas = ["3500"]
 partitions = ["random"]
 datasets = ["../datasets/covtype.csv", "../datasets/HIGGS.csv", "../datasets/connect-4Train.csv", "../datasets/reordered_mnist_train.csv"]
-#total executions per each configuration (match with nReps in classifierLoop.py)
+#total executions per each configuration
 nReps = 80
 for nData in nDatas:
     for partition in partitions:
@@ -17,4 +17,4 @@ for nData in nDatas:
                 statement = "python3 centralNode.py " + nNodes + " " + nData + " " + train + " " + test + " " + partition + " " + dataset
                 print("loop:", statement)
                 os.system(statement)
-                time.sleep(5)
+                time.sleep(6)
