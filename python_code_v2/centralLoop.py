843368bc98483f394e8035d5d4187a25ae6b3077
lidia <ruben.tome@udc.es>
2024-07-26 10:35:10 +0200

    error en centralLoop con reps

--- python_code_v2/centralLoop.py
+++ python_code_v2/centralLoop.py
@@ -11,13 +11,14 @@ datasets = ["../datasets/covtype.csv",
             "../datasets/HIGGS.csv",
             "../datasets/connect-4Train.csv",
             "../datasets/reordered_mnist_train.csv"]
+reps = "1"
 #total executions per each configuration
 nReps = 120
 for nData in nDatas:
     for partition in partitions:
         for dataset in datasets:
             for _ in range(nReps):
-                statement = "python3 centralNode.py " + nNodes + " " + nData + " " + train + " " + test + " " + partition + " " + dataset
+                statement = "python3 centralNode.py " + nNodes + " " + nData + " " + train + " " + test + " " + partition + " " + dataset + " " + reps
                 print("loop:", statement)
                 os.system(statement)
                 time.sleep(10)
