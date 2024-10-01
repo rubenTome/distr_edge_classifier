45d36f029dfeaeeea521275e10ae30d67b22d27a
rubenTome <rubentome27@gmail.com>
2024-10-01 12:24:38 +0200

    resultados divididos por dataset

--- python_code_v2/plot_graph.py
+++ python_code_v2/plot_graph.py
@@ -5,7 +5,8 @@ def parse_results(file):
     data =  {
         "balancedness": "",
         "distance": "",
-        "points": []
+        "points": [],
+        "datasets": []
     }
     f = open(file, 'r')
     lines = f.readlines()
@@ -16,41 +17,45 @@ def parse_results(file):
         data["distance"] = "br"
     else:
         data["distance"] = "en"
-    for i in range(2, len(lines) - 1, 8):
-        weight = lines[i].split(" ")[1].replace("\n", "")
-        acc = float(lines[i + 1].split(":")[1])
-        time = float(lines[i + 4].split(":")[1])
+    for i in range(1, len(lines) - 1, 8):
+        dataset = lines[i].split(" ")[6]
+        weight = lines[i + 1].split(" ")[1].replace("\n", "")
+        acc = float(lines[i + 2].split(":")[1])
+        time = float(lines[i + 5].split(":")[1])
         data["points"].append((weight, acc, time))
+        data["datasets"].append(dataset)
     f.close()
     return data
 
-def plot_graph(dataList):
+def plot_graph(dataList, selectedDs):
     colors = ["red", "green", "blue"]
-    markers = ["o", "^"]
+    marker = "o"
+    selectedW = "piwm"
     dist = []
+    w = {"piwm": "PIW"}
     for i in range(len(dataList)):
         color = colors[i]
         dist.append(dataList[i]["distance"])
         for j in range(len(dataList[i]["points"])):
-            marker = ""
-            if dataList[i]["points"][j][0] == "pnw":
-                marker = markers[0]
-            elif dataList[i]["points"][j][0] == "piwm":
-                marker = markers[1]
-            plt.scatter(dataList[i]["points"][j][1], dataList[i]["points"][j][2], s=60 ,color=color, marker=marker)
+            if (dataList[i]["points"][j][0] == selectedW 
+                and selectedDs in dataList[i]["datasets"][j]):
+                plt.scatter(dataList[i]["points"][j][1], dataList[i]["points"][j][2], s=60, color=color, marker=marker)
     #asume that all dataList elemnt have the same balancedness value
     if dataList[0]["balancedness"] == "perturbated":
-        plt.title("unbalanced scenario")
+        plt.title(selectedDs + " dataset, " 
+                  + "unbalanced scenario, " 
+                  + w[selectedW])
     else:
-        plt.title(dataList[0]["balancedness"] + " scenario")
+        plt.title(selectedDs + " dataset, " 
+                  + dataList[0]["balancedness"] + " scenario, " 
+                  + w[selectedW])
     plt.xlabel("Mean accuracy")
     plt.ylabel("Mean execution time")
-    w = {"o": "PNW", "^": "PIW"}
     d = {"red": "Energy", "green": "Canberra", "blue": "Bray-Curtis"}
     legend_elements = [
-        mlines.Line2D([], [], color=c, marker=m, linestyle='None',
-        markersize=10, label=f'{d[c]}, {w[m]}')
-        for m in markers for c in colors
+        mlines.Line2D([], [], color=c, marker=marker, linestyle='None',
+        markersize=10, label=f'{d[c]}')
+        for c in colors
     ]
     plt.legend(handles=legend_elements, loc='upper left')
     plt.show()
@@ -60,11 +65,13 @@ data = [
     parse_results("/home/ruben/FIC/GRADO/Q8/TFG/distr_edge_classifier/python_code_v2/results_3_nodes_ca/mean10_results_distr_10000_balanced.txt"),
     parse_results("/home/ruben/FIC/GRADO/Q8/TFG/distr_edge_classifier/python_code_v2/results_3_nodes_br/mean10_results_distr_10000_balanced.txt")
 ]
-plot_graph(data)
+for d in ["covtype", "HIGGS", "connect-4", "mnist"]:
+    plot_graph(data, d)
 
 data = [
     parse_results("/home/ruben/FIC/GRADO/Q8/TFG/distr_edge_classifier/python_code_v2/results_3_nodes/mean10_results_distr_10000_perturbated.txt"),
     parse_results("/home/ruben/FIC/GRADO/Q8/TFG/distr_edge_classifier/python_code_v2/results_3_nodes_ca/mean10_results_distr_10000_perturbated.txt"),
     parse_results("/home/ruben/FIC/GRADO/Q8/TFG/distr_edge_classifier/python_code_v2/results_3_nodes_br/mean10_results_distr_10000_perturbated.txt")
 ]
-plot_graph(data)
+for d in ["covtype", "HIGGS", "connect-4", "mnist"]:
+    plot_graph(data, d)
